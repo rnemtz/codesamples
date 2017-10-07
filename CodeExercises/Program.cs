d99373059b27e492ef4aaf47091be81fd3fbfd88
Rene Martinez <rmartinez@revspringinc.com>
2017-10-06 22:50:30 -0700

    Next Greater Element

--- CodeExercises/Program.cs
+++ CodeExercises/Program.cs
@@ -1,9 +1,34 @@
-﻿namespace CodeExercises
+﻿using System;
+
+namespace CodeExercises
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
+            var one = new[] { 4, 1, 2, 3 };
+            var two = new[] { 1, 3, 4, 2, 6, 7, 9, 8 };
+            var result = NextGreaterElement(one, two);
+
+           // Console.ReadLine();
+        }
+
+
+        //Next Greater Element
+        public static int[] NextGreaterElement(int[] findNums, int[] nums)
+        {
+            var result = new int[findNums.Length];
+            for (var i = 0; i < findNums.Length; i++)
+            {
+                result[i] = -1;
+                for (var j = Array.IndexOf(nums,findNums[i]); j < nums.Length; j++)
+                {
+                    if (nums[j] <= findNums[i]) continue;
+                    result[i] = nums[j];
+                    break;
+                }
+            }
+            return result;
         }
     }
 }
