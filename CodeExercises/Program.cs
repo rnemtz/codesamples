eb64b47bc6941fadffb8df9ad832b99fb918c7a9
Rene Martinez <rmartinez@revspringinc.com>
2017-10-06 23:08:01 -0700

    Fizz Buzz

--- CodeExercises/Program.cs
+++ CodeExercises/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CodeExercises
 {
@@ -8,11 +9,35 @@ namespace CodeExercises
         {
             var one = new[] { 4, 1, 2, 3 };
             var two = new[] { 1, 3, 4, 2, 6, 7, 9, 8 };
-            var result = NextGreaterElement(one, two);
+            var result = FizzBuzz(15);
 
-           // Console.ReadLine();
+            // Console.ReadLine();
         }
 
+        //Fizz Buz
+        public static IList<string> FizzBuzz(int n)
+        {
+            var list = new List<string>();
+            for (var c = 1; c <= n; c++)
+            {
+                var word = c.ToString();
+                
+                switch (c % 3)
+                {
+                    case 0 when c % 5 == 0:
+                        word = "FizzBuzz";
+                        break;
+                    case 0:
+                        word = "Fizz";
+                        break;
+                    default:
+                        if (c % 5 == 0) word = "Buzz";
+                        break;
+                }
+                list.Add(word);
+            }
+            return list;
+        }
 
         //Next Greater Element
         public static int[] NextGreaterElement(int[] findNums, int[] nums)
