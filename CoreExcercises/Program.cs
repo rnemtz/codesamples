7975eb20eb1423a78ea6cca9033214514c7237f3
Rene Martinez <renmarti@microsoft.com>
2020-01-30 22:11:06 -0800

    TreeDepth

--- CoreExcercises/Program.cs
+++ CoreExcercises/Program.cs
@@ -6,19 +6,46 @@ namespace CoreExcercises
     {
         private static void Main(string[] args)
         {
-            var bfs = new BreadthFirstSearch();
-            foreach (var c in bfs.GetEmployeeList())
-            {
-                Console.WriteLine(c);
-            }
-            Console.WriteLine("-------------------");
-            var dfs = new DepthFirstSearch();
-            foreach (var c in dfs.GetEmployeeList())
-            {
-                Console.WriteLine(c);
-            }
 
             Console.ReadKey();
         }
+
+        #region TreeDepth
+        /*
+            1. Find Minimum Depth of a Binary Tree
+            var node = new TreeNode(15)
+            {
+                Left = new TreeNode(5)
+                {
+                    Left = new TreeNode(3),
+                    Right = new TreeNode(7)
+                },
+                Right = new TreeNode(20)
+                {
+                    Left = new TreeNode(17),
+                    Right = new TreeNode(23)
+                    {
+                        Left = new TreeNode(22)
+                    }
+                }
+            };
+
+            var min = FindMinimumDepth(node);
+            var max = FindMaximumDepth(node);
+            Console.WriteLine($"Minimum: {min}, Maximum: {max}");
+        */
+
+
+        private static int FindMinimumDepth(TreeNode node)
+        {
+            return node == null ? -1 : Math.Min(FindMinimumDepth(node.Left), FindMinimumDepth(node.Right)) + 1;
+        }
+
+        private static int FindMaximumDepth(TreeNode node)
+        {
+            return node == null ? -1 : Math.Max(FindMaximumDepth(node.Left), FindMaximumDepth(node.Right)) + 1;
+        }
+        
+        #endregion
     }
 }
---
+++ CoreExcercises/TreeNode.cs
@@ -0,0 +1,14 @@
+﻿namespace CoreExcercises
+{
+    public class TreeNode
+    {
+        public TreeNode Left;
+        public TreeNode Right;
+        public int Val;
+
+        public TreeNode(int x)
+        {
+            Val = x;
+        }
+    }
+}
